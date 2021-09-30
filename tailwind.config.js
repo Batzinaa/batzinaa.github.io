8002e79fda6daf8b356279a60723542fbd09be71
batzinaa <ignaabat@gmail.com>
2021-09-30 14:26:02 +0300

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        bgcolor:"#0E1212",
+        acolor:"#7B9E9E",
+        bcolor:"#C4C4C4",
+        ccolor:"#4A5E5E",
+        dcolor:"#283333",
+      },
+      screens:{
+        "mobile-l":"425px",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
